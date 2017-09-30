7bf5848ba80874608a5c0594c66e71c501854ff4
AEtherC0r3 <master@aetherc0r3.eu>
2017-09-30 16:50:14 +0300

    Add enums to DataPoint and prettify rooms views

--- app/models/data_point.rb
+++ app/models/data_point.rb
@@ -2,5 +2,17 @@ class DataPoint < ApplicationRecord
   belongs_to :room
   belongs_to :node
 
-  validates_presence_of :room, :node
+  enum type: [:temperature, :humidity, :voc]
+
+  validates :room, presence: true
+  validates :node, presence: true
+  validates :type,
+            presence: true,
+            inclusion: {
+              in: DataPoint.types.keys
+            }
+
+  scope :temperature, -> { where(type: DataPoint.types[:temperature]) }
+  scope :humidity, -> { where(type: DataPoint.types[:humidity]) }
+  scope :voc, -> { where(type: DataPoint.types[:voc]) }
 end
